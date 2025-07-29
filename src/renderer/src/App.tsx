ac5d34a5630e99e99bae744e3e84b5607dd5033a
Marjon Godito <marjon.godito@gmanmi.com>
2025-07-30 00:13:16 +0800

    cleanups and bugfix

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,32 +1,18 @@
 import { useEffect, useMemo, useState } from 'react'
 import Layout from './components/layout'
-import { useWeather } from './hooks/weather'
 import { useGeo } from './hooks/ipapi'
 import { Sunrise, Sunset } from './components/sun-cards'
 import RealtimeClock from './components/realtime-clock'
-import { WeatherForecastApi } from './weather-sdk/forecast'
 import { AstronomicalData } from './weather-sdk/astronomical-data'
 
 function App(): React.JSX.Element {
   const daily = useMemo(() => ['sunrise', 'sunset'], [])
   const [pending, setPending] = useState<boolean>(true)
 
-  const [sunrise_sunset, setSun] = useState({
-    sunrise: '',
-    sunset: ''
-  })
-
   const [sunrise, setSunrise] = useState('')
   const [sunset, setSunset] = useState('')
 
   const { latitude, longitude, city, country } = useGeo()
-
-  const { weatherData, refetch } = useWeather({
-    long: 0,
-    lat: 0,
-    daily,
-    forecast_days: 1
-  })
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -73,27 +59,6 @@ function App(): React.JSX.Element {
       }
     )
   }, [longitude, latitude, daily])
-
-  useEffect(() => {
-    if (weatherData && !pending) {
-      const _sunrise = weatherData[0].weather_data.daily.sunrise[0]
-      const _sunset = weatherData[0].weather_data.daily.sunset[0]
-
-      console.log(weatherData[0])
-
-      console.log(_sunrise) // Tue Jul 29 2025 14:02:57 GMT+0800 (Philippine Standard Time)
-      setSun({
-        sunrise: new Date(_sunrise).toLocaleString('en-PH', {
-          hour: '2-digit',
-          minute: '2-digit'
-        }),
-        sunset: new Date(_sunset).toLocaleString('en-PH', {
-          hour: '2-digit',
-          minute: '2-digit'
-        })
-      })
-    }
-  }, [weatherData, pending])
 
   const greet = () => {
     const time = new Date().getHours()
